42a7ce9b813088ccf853f8da895048e36eaa2b8c
FauziaShafi <fauziashafi12@gmail.com>
2024-02-20 13:18:58 -0800

    Step 1 To 9

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,50 @@
-console.log("Js Connected")
+// Step:1 Convert the following highlighted identifiers to Camel Case notation:
+
+    // let someMonth;
+    // function theMonth();
+    // let currentMonth;
+    // let summerMonth;
+    // let MyLibraryFunction;
+
+
+// Step:2 Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression
+    // Numeric literal expression :  0146 
+    // String literal expression : "pizza"
+    // Boolean literal expression : true 
+    // Null literal expression : null 
+
+
+// Step:3 Give me two examples of complex / variable expressions.
+    // let aExpression = 50 - 20 ;
+
+
+// Step:4 Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
+    // let firstName, lastname, address , city , state, zipCode , yourAge, referralSource, mayWeContactYou ;
+
+
+// Step:5 Take the 3 of the 9 variables that you created above and demonstrate 3 ways for declaring and assigning values to those variables.
+    // let firstName = "Abhi";
+    // let state = "CA";
+    // let zipCode = 92128 ;
+
+// Step:6 Create a variable.
+//Add a number and a string and display the coerced result in the browser’s console window.
+
+    // let number = "20 Pen";
+    // console.log(`Hi!!! My name is Ana and I have ${number}`)
+
+// Step: 7 : Create two variables.
+// For the first variable, add a Boolean and a string and display the coerced result.
+// For the second variable, add a number and a Boolean and display the coerced result.
+        // let a = true;
+        // let b = 12;
+        // console.log(`The output of this code should be a boolean = ${a} and a string`);
+        //   console.log(`The output of this code should be a number = ${b} and a boolean = ${a}`);
+
+// Step: 8 Is the following string literal valid? If not, how would you fix it?
+
+    // let someString = "Who once said,' Only two things are infinite, the universe and human stupidity, and I'm not sure about the former'.";
+    // console.log(someString);
+ 
+
+// Step:9 : Create a variable that produces a null value in the console window.Then, create a variable that produces an undefined value in the console window.
